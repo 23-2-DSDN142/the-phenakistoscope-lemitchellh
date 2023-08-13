4e33bf84276665da1626741f0b81381b90e1c092
lemitchellh <132876848+lemitchellh@users.noreply.github.com>
2023-08-14 10:17:59 +1200

    Messing around

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,79 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(500));
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 100, 50, 90);  //lets us draw the whole circle background, ignoring the boundaries
+
 
   var layer1 = new PLayer(faces);
   layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  layer1.set_boundary( 200, 1000);
+
+  var layer4 = new PLayer(faces2);
+  layer1.mode( SWIRL(6) );
+  layer1.set_boundary( 1000, 300);
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
+
+  var layer3 = new PLayer(diamonds);
+  layer3.mode( SWIRL(9) );
+  layer3.set_boundary( 600, 100 );
+
+
 }
 
 function faces(x, y, animation, pScope){
   
   scale(animation.frame*2);
+  fill (190, 170, 190);
+  rect(5,5,50,50); // draw head
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+}
 
+function faces2 (x, y, animation, pScope){
+  scale( animation.frame*4.5);
+  fill (50, 90, 90);
+  rect(10,30-animation.wave(.5)*10,50,50)
 }
 
 function squares(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
+  let angleOffset = (360 / SLICE_COUNT) / 1
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  fill(150, 150, 245)
+  arc(x,y,200,200,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
+  fill(0)
+  rect(-10,-30-animation.wave(.5)*10,20,20) // .wave is a cosine wave btw
 }
+  
+function diamonds(x, y, animation, pScope){
+
+  let angleOffset = (360 / SLICE_COUNT) / .7
+  let backgroundArcStart = 180 - angleOffset;
+  let backgroundArcEnd = 360 + angleOffset;
+
+scale (animation.frame*2);
+
+  fill (255, 100, 110)
+  arc(x + 250,y + 250,700,700,backgroundArcEnd,backgroundArcStart); // draws "pizza slice" in the background
+
+
+ 
+
+  
+  }
+
